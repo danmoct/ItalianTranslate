9e5dc13a42c2e18289b1e3f9190b0788a1aea811
Daniel Moctezuma <danmoct@yahoo.com>
2019-09-03 10:09:50 -0500

    Update for Italian Translate

--- SRC/test2.py
+++ SRC/test2.py
@@ -5,13 +5,13 @@ from os import path
 
 
 def text_Read():
-	imPath = input("Enter a filename to read: ")
+	imPath = input("\nEnter a filename to read: ")
 	fPath = path.join('../IMG',imPath)
 	img = cv2.imread(fPath)
 	cv2.imshow("picture", img)
 	cv2.waitKey(0)
 	text = pytesseract.image_to_string(img)
-	save = input("Enter a filename to save:")
+	save = input("\nEnter a filename to save:")
 	sPath = path.join('../Italian',save)
 	file = open(sPath, "w")
 	file.write(text)
@@ -20,31 +20,38 @@ def text_Read():
 
 def tTranslate():
 	translator = Translator()
-	file = input("Enter a filename to read: ")
+	file = input("\nEnter a filename to read: ")
 	fPath = path.join('../Italian',file)
 	file = open(fPath, 'r')
 	text = file.read()
 	text2 = (translator.translate(text, src = 'it' ,dest = 'en').text)
 	print(text2)
-	save = input("Enter a filename to save:")
+	save = input("\nEnter a filename to save:")
 	sPath = path.join('../English',save)
 	file = open(sPath,'w')
 	file.write(text2)
 	file.close()
 
 def main():
-	print("\t\t\t\t\t\tMenu")
-	print("---------------------------")
-	print("1. Image to Text")
-	print("2. Translate")
+	while True:
+		print("\t\t\t\t\t\t\n\nMenu")
+		print("---------------------------")
+		print("1. Image to Text")
+		print("2. Translate")
+		print("3. Exit")
 
-	value = int(input("Select an option: "))
-	while value != 1 and value !=2:
+	
+	
 		value = int(input("Select an option: "))
-	if value == 1:
-		text_Read()
-	if value == 2:
-		tTranslate()
+		if value != 1 and value !=2 and value!=3:
+			value = int(input("Select an option: "))
+		if value == 1:
+			text_Read()
+		if value == 2:
+			tTranslate()
+		if value == 3:
+			break
+
 
 if __name__ == "__main__":
 	main()
